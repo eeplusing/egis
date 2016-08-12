953298eb80fa7ba0c5328e2fec887548a4a83a5f
eeplusing <eplusing@gmail.com>
2016-08-12 14:51:15 +0800

    根据条件更新数据， 更新属性项中不能包含空对象null（可以使空字符创""）

--- egis/WebRoot/ol3Demo/filter/filter.js
+++ egis/WebRoot/ol3Demo/filter/filter.js
@@ -245,7 +245,7 @@ $('#btnZoomOut').on('click', function() {
 	var newResolution = view.constrainResolution(view.getResolution(), -1);
 	view.setResolution(newResolution);
 	});
-$('#btnTest').on('click', function() {
+$('#btnIntersectTest').on('click', function() {
 	/**
 	 * 以多边形是否相交查询
 	 * */
@@ -318,3 +318,333 @@ $('#btnTest').on('click', function() {
 	}); 
 });
 
+
+$('#btnQueryByIDTest').on('click', function() {
+	/**
+	 * 以属性条件查询
+	 * */
+	/*var requestData = 
+		"<wfs:GetFeature service='WFS' version='1.1.0' " + 
+		"xmlns:hanzhong='http://www.eplusing.com/hanzhong' " +
+		"xmlns:wfs='http://www.opengis.net/wfs' " + 
+		"xmlns='http://www.opengis.net/ogc' " + 
+		"xmlns:gml='http://www.opengis.net/gml' " + 
+		"xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' " + 
+		"xsi:schemaLocation='http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/WFS-basic.xsd' " + 
+		"outputFormat='application/json'> " + 
+		"<wfs:Query typeName='hanzhong:djq'> " + 
+		"<wfs:PropertyName>hanzhong:djqmc</wfs:PropertyName> " + 
+		"<Filter> " + 
+			"<Intersects> " + 
+				"<PropertyName>geometry</PropertyName> " +
+				"<gml:Polygon srsName='http://www.opengis.net/gml/srs/epsg.xml#2360'> " +
+					"<gml:exterior> " +
+				     	"<gml:LinearRing> " +
+				     		"<gml:posList>402314.93545 3684110.69228 406476.09007 3682430.96014 403345.68017 3685523.19431 402314.93545 3684110.69228</gml:posList> " +
+				        "</gml:LinearRing> " +
+				    "</gml:exterior> " +
+			    "</gml:Polygon> " +
+			"</Intersects> " +
+		"</Filter> " +
+		"</wfs:Query>" + 
+		"</wfs:GetFeature>";*/
+	var requestData = 
+		"<wfs:GetFeature service='WFS' version='1.1.0' " + 
+		"xmlns:hanzhong='http://www.eplusing.com/hanzhong' " +
+		"xmlns:wfs='http://www.opengis.net/wfs' " + 
+		"xmlns='http://www.opengis.net/ogc' " + 
+		"xmlns:gml='http://www.opengis.net/gml' " + 
+		"xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' " + 
+		"xsi:schemaLocation='http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/WFS-basic.xsd' " + 
+		"outputFormat='application/json'> " + 
+		"<wfs:Query typeName='hanzhong:syqzd_db'> " + 
+			//"<wfs:PropertyName>hanzhong:qlrxm</wfs:PropertyName> " + 
+			"<Filter> " + 
+				"<PropertyIsEqualTo>" + 
+				"<PropertyName>chid</PropertyName>" + 
+				"<Literal>" + "fc1b64cef9d74245a4aadd18004fc1d9" + "</Literal>" + 
+				"</PropertyIsEqualTo>" + 
+			"</Filter> " +
+		"</wfs:Query>" + 
+		"</wfs:GetFeature>";
+	$.ajax({ 
+			url:'http://localhost:9000/geoserver/wfs',
+			type: 'POST',
+			contentType: 'text/xml',
+			dataType: 'json',
+			processData: false,
+			data: requestData,
+			error: function(data){ //失败 
+				alert('Error loading document'); 
+				alert(data);
+			}, 
+			success: function(msg){ //成功 
+				console.log(msg);
+				console.log(msg.features.length);
+				//JSON.parse(jsonstr); //可以将json字符串转换成json对象 
+				//JSON.stringify(jsonobj); //可以将json对象转换成json对符串 
+				//alert( "return message: " + JSON.stringify(msg) ); 
+				//console.log( "return message: " + JSON.stringify(msg) );
+				console.log(JSON.stringify(msg));
+			} 
+	}); 
+});
+
+
+
+$('#btnInsertTest').on('click', function() {
+	var requestData = "<Transaction xmlns='http://www.opengis.net/wfs' service='WFS' version='1.1.0' " + 
+		"xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' " + 
+		"xsi:schemaLocation='http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd'>" +
+	"<Insert>" + 
+		"<syqzd_db xmlns='http://www.eplusing.com/hanzhong'>" + 
+			"<geometry>" + 
+				"<MultiPolygon xmlns='http://www.opengis.net/gml'>" + 
+					"<polygonMember>" + 
+						"<Polygon srsName='http://www.opengis.net/gml/srs/epsg.xml#2360'>" + 
+							"<exterior><LinearRing><posList>" + 
+							"36427663.76193 3666473.50481 36430946.87475 3669756.61763 36421173.88775 3679147.84232 36427663.76193 3666473.50481" + 
+							"</posList></LinearRing></exterior>" + 
+						"</Polygon>" + 
+					"</polygonMember>" + 
+				"</MultiPolygon>" + 
+			"</geometry>" +
+			"<zdmj>2342.45</zdmj>" + 
+			"<zddm>6233040</zddm>" + 
+			"<bdcdyh>6233040893GB489W00</bdcdyh>" + 
+			"<zdszb>北至</zdszb>" + 
+			"<zdszd>东至</zdszd>" + 
+			"<zdszn>南至</zdszn>" + 
+			"<zdszx>西至</zdszx>" + 
+			"<zdszb>北至</zdszb>" + 
+		"</syqzd_db>" + 
+	"</Insert>" + 
+"</Transaction>";
+	alert(requestData);
+	$.ajax({ 
+		url:'http://localhost:9000/geoserver/wfs',
+		type: 'POST',
+		contentType: 'text/xml',
+		dataType: 'json',
+		processData: false,
+		data: requestData,
+		error: function(data){ //失败 
+			alert('Error loading document'); 
+			alert(data);
+		}, 
+		success: function(msg){ //成功 
+			console.log(msg);
+			console.log(JSON.stringify(msg));
+		} 
+	}); 
+});
+
+$('#btnUpdateTest').on('click',function updateData(){
+	var zddm = "610702500003GB00006";
+	var bdcdyh = "610702500003GB00006W8888888";
+	var zdtzm = "B";
+	var zl = "汉台区兴元新区未知名称186";
+	var zdmj = "1171.1200";
+	var mjdw = "1";
+	var yt = "住宅用地";
+	var dj = "";
+	var jg = "";
+	var chid = "ae57606df8fa441c93dc541d5aca300f";
+	var layerName = "syqzd_db";
+	
+	//更新GISDB
+	var requestData = "<Transaction xmlns='http://www.opengis.net/wfs' service='WFS' version='1.1.0' " + 
+		"xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' " + 
+		"xsi:schemaLocation='http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd'>" + 
+		"<Update typeName='feature:" + layerName  + "' xmlns:feature='http://www.eplusing.com/hanzhong'>" +
+			"<Property>" +   
+		       	"<Name>" + "zddm" + "</Name>" + 
+		       	"<Value>" + zddm + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "bdcdyh" + "</Name>" + 
+		       	"<Value>" + bdcdyh + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zdtzm" + "</Name>" + 
+		       	"<Value>" + zdtzm + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zl" + "</Name>" + 
+		       	"<Value>" + zl + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zdmj" + "</Name>" + 
+		       	"<Value>" + zdmj + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "mjdw" + "</Name>" + 
+		       	"<Value>" + mjdw + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "yt" + "</Name>" + 
+		       	"<Value>" + yt + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "dj" + "</Name>" + 
+		       	"<Value>" + "</Value>" + 
+			"</Property>" +  
+			"<Property>" +   
+		       	"<Name>" + "jg" + "</Name>" + 
+		       	"<Value>" + jg + "</Value>" + 
+			"</Property>" +   
+			"<Filter xmlns='http://www.opengis.net/ogc'>" + 
+				"<PropertyIsEqualTo>" + 
+					"<PropertyName>" + "chid" +"</PropertyName>" + 
+					"<Literal>" + chid + "</Literal>" + 
+				"</PropertyIsEqualTo>" + 
+			"</Filter>" + 
+		"</Update>" + 
+	"</Transaction>";
+	$.ajax({ 
+		url:'http://localhost:9000/geoserver/wfs',
+		type: 'POST',
+		contentType: 'text/xml',
+		processData: false,
+		data: requestData,
+		error: function(data){ //失败 
+			alert("更新GIS信息失败：" + data); 
+		}, 
+		success: function(msg){ //成功 
+			alert("更新GIS信息成功:" + msg);
+		} 
+	});
+	
+	
+});
+
+//按条件更新gis
+/*$('#btnUpdateTest').on('click',function updateData(){
+	var requestData = "<Transaction xmlns='http://www.opengis.net/wfs' service='WFS' version='1.1.0' " + 
+	"xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' " + 
+	"xsi:schemaLocation='http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd'>" + 
+		"<Update typeName='feature:" + "syqzd_db"  + "' xmlns:feature='http://www.eplusing.com/hanzhong'>" +
+			"<Property>" +   
+		       	"<Name>" + "zddm" + "</Name>" + 
+		       	"<Value>" + "610702008205GB00056" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "bdcdyh" + "</Name>" + 
+		       	"<Value>" + "610702008205GB00056W00000001" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zdtzm" + "</Name>" + 
+		       	"<Value>" + "B" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zl" + "</Name>" + 
+		       	"<Value>" + "汉台区北关街道办事处付家巷村委会汉台区前进东路南侧" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zdmj" + "</Name>" + 
+		       	"<Value>" + "434.000000000" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "mjdw" + "</Name>" + 
+		       	"<Value>" + mjdw + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "yt" + "</Name>" + 
+		       	"<Value>" + "房屋" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "dj" + "</Name>" + 
+		       	"<Value>" + dj + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "jg" + "</Name>" + 
+		       	"<Value>" + "0.00000000000" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "qllx" + "</Name>" + 
+		       	"<Value>" + "3" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "qlxz" + "</Name>" + 
+		       	"<Value>" + "102" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "qlsdfs" + "</Name>" + 
+		       	"<Value>" + "2" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "rjl" + "</Name>" + 
+		       	"<Value>" + "0" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "jzmd" + "</Name>" + 
+		       	//"<Value>" + "0" + "</Value>" + 
+		       	"<Value>" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "jzxg" + "</Name>" + 
+		       	"<Value>" + "0" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zdszd" + "</Name>" + 
+		       	"<Value>" + "以界址点连线为界，邻久麟铭座" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zdszn" + "</Name>" + 
+		       	"<Value>" + "以界址点连线为界，邻久麟铭座" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zdszx" + "</Name>" + 
+		       	"<Value>" + "以界址点连线为界，邻久麟铭座" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zdszb" + "</Name>" + 
+		       	"<Value>" + "以界址点连线为界，邻久麟铭座" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zdt" + "</Name>" + 
+		       	"<Value>" + "|/ODM/userfiles/09599e0cc6bb4e3f9da030923b83466e/files/reg/base/regBaseChxx/2016/07/久麟银座.rar|/ODM/userfiles/09599e0cc6bb4e3f9da030923b83466e/files/reg/base/regBaseChxx/2016/07/久麟银座-Model.jpg" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "tfh" + "</Name>" + 
+		       	"<Value>" + "A0" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "djh" + "</Name>" + 
+		       	"<Value>" + djh + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "zt" + "</Name>" + 
+		       	"<Value>" + zt + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "qlrxm" + "</Name>" + 
+		       	"<Value>" + "小明" + "</Value>" + 
+			"</Property>" +   
+			"<Property>" +   
+		       	"<Name>" + "islogout" + "</Name>" + 
+		       	"<Value>" + "0" + "</Value>" + 
+			"</Property>" +   
+
+			"<Filter xmlns='http://www.opengis.net/ogc'>" + 
+				"<PropertyIsEqualTo>" + 
+					"<PropertyName>chid</PropertyName>" + 
+					"<Literal>" + "ae57606df8fa441c93dc541d5aca300f" + "</Literal>" + 
+				"</PropertyIsEqualTo>" + 
+			"</Filter>" + 
+		"</Update>" + 
+	"</Transaction>";
+	$.ajax({ 
+		url:'http://localhost:9000/geoserver/wfs',
+		type: 'POST',
+		contentType: 'text/xml',
+		processData: false,
+		data: requestData,
+		error: function(data){ //失败 
+			alert("更新GIS信息失败：" + data); 
+		}, 
+		success: function(msg){ //成功 
+			alert("更新GIS成功:" + msg);
+		} 
+	}); 
+});*/
